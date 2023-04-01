f94c57f8cfbc4d5fa664843e63505682c00828fa
Okiki <okikio.dev@gmail.com>
2023-04-01 05:26:33 +0000

    chore: ...

--- run-with-compressed-wasm.js
+++ run-with-compressed-wasm.js
@@ -1,7 +1,7 @@
 import init, { new_url, set_pathname_url, get_host_url, get_origin } from "./pkg/hello_wasm.js";
 import { source } from "./pkg/wasm_compressed.js"
 
-await init(await source());
+await init(source());
 const url_id = new_url("http://google.com:80\\\\@yahoo.com/");
 set_pathname_url(url_id, "./cool/there/is/indeed a path");
 console.log({
